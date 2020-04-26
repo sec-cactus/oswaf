f87d7ffa89986e6e748a575fae8b7916d284fb8a
sec-cactus <dalbert@163.com>
2020-04-26 19:41:33 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,44 @@
+--WAF config file,enable = "on",disable = "off"
+
+--waf status
+config_waf_enable = "on"
+--log dir
+config_log_dir = "/var/log/ngx_lua_waf"
+--rule setting
+config_rule_dir = "/usr/local/openresty/nginx/conf/waf/lua/rule-config"
+--enable/disable white url
+config_white_url_check = "on"
+--enable/disable white ip
+config_white_ip_check = "on"
+--enable/disable block ip
+config_black_ip_check = "on"
+--enable/disable url filtering
+config_url_check = "on"
+--enalbe/disable url args filtering
+config_url_args_check = "on"
+--enable/disable user agent filtering
+config_user_agent_check = "on"
+--enable/disable cookie deny filtering
+config_cookie_check = "on"
+--enable/disable cc filtering
+config_cc_check = "on"
+--cc rate the xxx of xxx seconds
+config_cc_rate = "10/60"
+--enable/disable post filtering
+config_post_check = "on"
+--config waf output redirect/html
+config_waf_output = "html"
+--if config_waf_output ,setting url
+config_waf_redirect_url = "https://www.unixhot.com"
+config_output_html=[[
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="zh-cn" />
+<title>网站防火墙</title>
+</head>
+<body>
+<h1 align="center"> 欢迎白帽子进行授权安全测试，安全漏洞请联系QQ：1111111。
+</body>
+</html>
+]]
